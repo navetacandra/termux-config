393d802335383e7652e199d0b42de2c6558cd139
navetacandra <70505125+navetacandra@users.noreply.github.com>
2022-06-03 20:06:46 +0700

    Add ts-node package to global node dependencies

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ pkg install x11-repo -y
 
 # install dependencies from packages
 gem install lolcat
-npm install typescript eslint --global
+npm install typescript ts-node eslint --global
 proot-distro install ubuntu
 
 # re-update and re-upgrade dependencies
